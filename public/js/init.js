f3b648a65b7dde693ad4dd89fea947722362a33c
James Farrell <jamesfarrellmusic@gmail.com>
2019-04-05 16:15:33 -0500

    initial commit

---
+++ public/js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+    $('.carousel').carousel();
+    $('.fixed-action-btn').floatingActionButton();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
