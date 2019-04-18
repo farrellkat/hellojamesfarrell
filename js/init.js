89d5c757b8ca262e74567b4596ef367b3ce1437e
James Farrell <jamesfarrellmusic@gmail.com>
2019-04-18 15:21:06 -0500

    built sticky header

--- js/init.js
+++ js/init.js
@@ -7,6 +7,21 @@
     $('.carousel').carousel();
     $('.fixed-action-btn').floatingActionButton();
     $('.slider').slider();
-  }); // end of document ready
+    $(function(){
+      var stickyHeaderTop = $('#stickytypeheader').offset().top;
+
+      $(window).scroll(function(){
+              if( $(window).scrollTop() > stickyHeaderTop ) {
+                      $('#stickytypeheader').css({position: 'fixed', top: '0px', backgroundColor: "black", padding:"5px"});
+                      $('.socialIcons').css({color: "white"})
+                      $('#sticky').css('display', 'block');
+              } else {
+                      $('#stickytypeheader').css({position: 'static', top: '0px', backgroundColor: "white"});
+                      $('.socialIcons').css({color: "black"})
+                      $('#sticky').css('display', 'none');
+              }
+      });
+  });
+}); // end of document ready
 })(jQuery); // end of jQuery name space
 
